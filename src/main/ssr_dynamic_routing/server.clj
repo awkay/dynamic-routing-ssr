b82d109e4b03abb65929d41d90f32df469b7fe57
Tony Kay <tony.kay@gmail.com>
2018-04-19 18:21:05 -0700

    added comments

--- src/main/ssr_dynamic_routing/server.clj
+++ src/main/ssr_dynamic_routing/server.clj
@@ -17,26 +17,27 @@
 (defn build-ui-tree [match]
   (let [client-db (ssr/build-initial-state (prim/get-initial-state root/Root {}) root/Root)
         final-db  (-> client-db
+                    ;; CRITICAL: Install the routes, or their state won't be in the db
                     (r/install-route* :main root/Main)
                     (r/install-route* :other other/Other)
                     (r/route-to* match))]
+    ;; CRITICAL: Pass the final database to get-query!!! Or you won't get the updated dynamic query
     (prim/db->tree (prim/get-query root/Root final-db) final-db final-db)))
 
 (defn server-side-render [env {:keys [handler] :as match}]
   (let [ui-tree (build-ui-tree match)
         html    (dom/render-to-str ((prim/factory root/Root) ui-tree))]
-    (println ui-tree)
-    (->
-      (resp/response (str
-                       "<html><body><div id='app'>"
-                       html
-                       "</div></body></html>"))
+    (-> (resp/response (str
+                         "<html><body><div id='app'>"
+                         html
+                         "</div></body></html>"))
       (resp/content-type "text/html"))))
 
 (defn build-server
   [{:keys [config] :or {config "config/dev.edn"}}]
   (make-fulcro-server
     :parser-injections #{:config}
+    ;; Quick way to hack a couple of pages in with different match handlers
     :extra-routes {:routes   ["/" {"main.html"  :main
                                    "other.html" :other}]
                    :handlers {:main  server-side-render
